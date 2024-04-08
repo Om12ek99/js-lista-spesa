4be0f5187bfd751c0cea5edd28927c1416c3d890
Om12ek99 <omar.elkamili@gmail.com>
2024-04-08 21:23:10 +0200

    add:script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-             
+// INPUT
+// array xon la lista della spesa
+const shoppingItems = ["latte", "biscotti", "tè", "pane"];
+
+// dichiarazione variabili
+let shoppingItemsElem = document.querySelector(".shopping");
+let i = 0                                           // declariation increment variable
+let shoppingItem ="";
+
+
+
+
+
+// comando while
+while(i < shoppingItems.length){                         
+    shoppingItem +="<li>" + shoppingItems[i] + "</li>";  
+    i++;                                                
+}
+
+// OUTPUT
+shoppingItemsElem.innerHTML = shoppingItem;              
